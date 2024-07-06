562e922a9c36299716c16097f692f8ce3d667cf1
annaagabi <annaa0478@gmail.com>
2024-07-06 15:46:47 -0300

    feat: added console methods and join string with variable

--- src/helpers/fundamentos.js
+++ src/helpers/fundamentos.js
@@ -1,1 +1,15 @@
-console.log('Oi nerdola')
+console.log("Oi nerdola")
+
+let error = "Não deu certo nerdola"
+
+let info = "Info"
+
+console.error("error", error)
+console.error(`error: ${error}`)
+
+console.info("Nerd", info)
+
+// Limpa uma parte do código/ console que vc não quer que ninguém veja
+console.clear("Limpado")
+
+console.log("Voltei")
