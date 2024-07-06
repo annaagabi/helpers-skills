f43b0dc3106f8846c25859978c60ae2622c305c4
annaagabi <annaa0478@gmail.com>
2024-07-06 16:36:12 -0300

    feat: created innerhtml helper

---
+++ src/helpers/innerhtml.js
@@ -0,0 +1,23 @@
+function handleGengar(){
+    const pokemon = "https://pokeapi.co/api/v2/pokemon/gengar"
+
+    fetch(pokemon)
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data)
+            document.getElementById("container").innerHTML = 
+            `<div style = "
+                display: flex; 
+                flex-direction: column; 
+                align-itens: center; 
+                justify-content: center;
+                border: 1px solid black;
+            ">
+                <p>${data.name}</p>
+                <img src=${data.sprites.front_default} alt="gengar img">
+            </div>`
+        })
+        .catch((error) => console.error("Pokemon não encontrado", error))
+}
+
+handleGengar()
